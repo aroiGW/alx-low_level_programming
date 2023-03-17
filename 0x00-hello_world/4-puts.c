1e2f3b851fae1244b2a6118bfa1a3ece91e8a56f
aroiGW <gongreenwolf@gmail.com>
2023-03-17 08:38:11 -0700

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ * main - this is the main function
+ *
+ * return: always 0
+ */
 
-int main(void)
-{
-    puts("\"Programming is like building a multilingual puzzle");
+int main(void) {
+    puts("/"Programming is like building a multilingual puzzle");
     return 0;
 }
