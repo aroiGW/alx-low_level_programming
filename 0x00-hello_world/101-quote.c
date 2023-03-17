a8c2cf65b456697f3389cb7df58024bb5c20c5b4
aroiGW <gongreenwolf@gmail.com>
2023-03-17 15:37:36 -0700

    basically a simple

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry
+ * Return: 1
+ */
+int main(void)
+{
+	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, message, 38);
+	return (1);
+}
