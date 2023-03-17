2914cfb0e2c33e76fc5d7038f5df2b250078c9b1
aroiGW <gongreenwolf@gmail.com>
2023-03-17 15:44:05 -0700

    basically a simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDERR_FILENO, message, 38);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
