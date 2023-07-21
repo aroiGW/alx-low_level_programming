3cc55892d1176b1c6481b32c135b068d38297400
aroiGW <gongreenwolf@gmail.com>
2023-07-21 12:36:24 -0700

    try

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints the alphabet
+ */
+
+void print_alphabet(void);
+{
+	char letter;
+	for(letter=a;letter<=z;letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
