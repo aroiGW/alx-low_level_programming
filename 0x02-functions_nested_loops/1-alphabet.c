4585f085519e214e49215ed4b04d816c17f1f507
aroiGW <gongreenwolf@gmail.com>
2023-07-21 12:56:32 -0700

    try

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,12 @@
  * main - prints the alphabet
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char letter;
-	for(letter=a;letter<=z;letter++)
+
+	for (letter='a';letter<='z';letter++)
 		_putchar(letter);
+
 	_putchar('\n');
 }
